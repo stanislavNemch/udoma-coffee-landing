adb13d23555726226a2010fd0909a1c8cb620bbf
Stanislav Nemchenko <stas.buka@gmail.com>
2026-08-28 00:03:54 +0300

    fix: remove process usage from vite config for ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/udoma-coffee-landing/" : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/udoma-coffee-landing/" : "/",
   plugins: [react()],
-});
+}));
